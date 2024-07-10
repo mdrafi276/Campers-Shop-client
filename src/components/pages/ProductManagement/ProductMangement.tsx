3c131975bee9a77bc586bbaf3eba642a3cd284f3
mdrafi276 <mdrafi37473@gmail.com>
2024-07-11 01:23:59 +0600

    add video sectino

--- src/components/pages/ProductManagement/ProductMangement.tsx
+++ src/components/pages/ProductManagement/ProductMangement.tsx
@@ -52,58 +52,80 @@ const ProductManagement = () => {
     };
 
     return (
-        <div className="max-w-screen-xl mx-auto py-20 px-3 ">
-            <div className="border border-white rounded-lg p-8">
-                <div className="pb-6 flex items-center justify-between border-b border-white">
-                    <Link to="/create-product">
-                        <Button className="bg-green-500 hover:bg-green-600">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="size-6"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                                />
-                            </svg>
-                            Create A New Product
-                        </Button>
-                    </Link>
-                    <h2 className="text-xl text-white font-bold">
-                        Total Product: {data?.data?.length}
-                    </h2>
-                </div>
-                <Table className="p-12">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px] text-white">Image</TableHead>
-                            <TableHead className='text-white'>Name</TableHead>
-                            <TableHead className='text-white'>Price</TableHead>
-                            <TableHead className='text-white'>Category</TableHead>
-                            <TableHead className="text-right text-white">Actions</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data?.data?.map((item) => (
-                            <TableRow key={item._id}>
-                                <TableCell>
-                                    <img
-                                        className="object-cover rounded-lg size-14"
-                                        src={item.image}
-                                        alt={item.name}
+        <div className='bg-[#001945]'>
+            <div className="max-w-screen-xl  mx-auto py-20 px-3 ">
+                <div className="border border-white rounded-lg p-8">
+                    <div className="pb-6 flex items-center justify-between border-b border-white">
+                        <Link to="/create-product">
+                            <Button className="bg-[#c800d3] hover:bg-[#a602ae]">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                                     />
-                                </TableCell>
-                                <TableCell className="text-sm text-white">{item.name}</TableCell>
-                                <TableCell className='text-white'>{item.price}</TableCell>
-                                <TableCell className='text-white'>{item.category}</TableCell>
-                                <TableCell className="text-right text-white space-x-2">
-                                    <Link to={`/update-product/${item._id}`}>
-                                        <Button className="bg-green-500 hover:bg-green-600">
+                                </svg>
+                                Create A New Product
+                            </Button>
+                        </Link>
+                        <h2 className="text-xl text-white font-bold">
+                            Total Product: {data?.data?.length}
+                        </h2>
+                    </div>
+                    <Table className="p-12">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px] text-white">Image</TableHead>
+                                <TableHead className='text-white'>Name</TableHead>
+                                <TableHead className='text-white'>Price</TableHead>
+                                <TableHead className='text-white'>Category</TableHead>
+                                <TableHead className="text-right text-white">Actions</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data?.data?.map((item) => (
+                                <TableRow key={item._id}>
+                                    <TableCell>
+                                        <img
+                                            className="object-cover rounded-lg size-14"
+                                            src={item.image}
+                                            alt={item.name}
+                                        />
+                                    </TableCell>
+                                    <TableCell className="text-sm text-white">{item.name}</TableCell>
+                                    <TableCell className='text-white'>{item.price}</TableCell>
+                                    <TableCell className='text-white'>{item.category}</TableCell>
+                                    <TableCell className="text-right text-white space-x-2">
+                                        <Link to={`/update-product/${item._id}`}>
+                                            <Button className="bg-green-500 hover:bg-green-600">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="currentColor"
+                                                    className="size-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                                    />
+                                                </svg>
+                                            </Button>
+                                        </Link>
+
+                                        <Button
+                                            onClick={() => handleDelete(item._id)}
+                                            variant={"destructive"}
+                                        >
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 fill="none"
@@ -115,38 +137,19 @@ const ProductManagement = () => {
                                                 <path
                                                     strokeLinecap="round"
                                                     strokeLinejoin="round"
-                                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                                                 />
                                             </svg>
                                         </Button>
-                                    </Link>
-
-                                    <Button
-                                        onClick={() => handleDelete(item._id)}
-                                        variant={"destructive"}
-                                    >
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="currentColor"
-                                            className="size-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                            />
-                                        </svg>
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         </div>
+
     );
 };
 
